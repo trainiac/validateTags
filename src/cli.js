76b9329448a417e1ee4f890d4279ccd78be4ac34
trainiac <adkison.adrian@gmail.com>
2017-05-01 19:02:27 -0700

    add support for list of files

--- src/cli.js
+++ src/cli.js
@@ -12,37 +12,64 @@ const formatters = {
   string: formatterFunction
 }
 
+const flatten = array => {
+  if (!array || !array.length) {
+    return []
+  }
+
+  let index = -1
+  const length = array.length
+  const result = []
+
+  while (++index < length) {
+    const value = array[index]
+    if (Array.isArray(value)) {
+      let index2 = -1
+      const valueLength = value.length
+      const offset = result.length
+      while (++index2 < valueLength) {
+        result[offset + index2] = value[index2]
+      }
+    } else {
+      result[result.length] = value
+    }
+  }
+  return result
+}
+
 function readFile (filePath) {
   return new Promise((resolve, reject) => {
     fs.readFile(filePath, 'utf8', (err, content) => {
       if (err) {
-        return reject(err)
+        reject(err)
       }
       resolve(content)
     })
   })
 }
 
-const prepareReturnValue = formatter => validateTagsResults => {
+const prepareReturnValue = formatter => results => {
+  const flattened = flatten(results)
   return new Promise(resolve => {
-    const errored = validateTagsResults.some(result => result.errors.length)
+    const errored = flattened.some(result => result.errors.length)
 
     const returnValue = {
       errored,
-      output: formatter(validateTagsResults)
+      output: formatter(flattened)
     }
 
     resolve(returnValue)
   })
 }
 
-function validateTagsForFiles (fileList, options) {
-  return globby(fileList).then(filePaths => {
-    const getValidateTagsResults = filePaths.map(filePath => {
-      const absoluteFilepath = (!path.isAbsolute(filePath))
-        ? path.join(process.cwd(), filePath)
-        : filePath
+function getAbsolutePath (filePath) {
+  return (!path.isAbsolute(filePath)) ? path.join(process.cwd(), filePath) : filePath
+}
 
+function validateTagsForFile (file) {
+  return globby(file).then(filePaths => {
+    const getValidateTagsResults = filePaths.map(filePath => {
+      const absoluteFilepath = getAbsolutePath(filePath)
       return readFile(absoluteFilepath).then(validateTags).then(result => {
         return {
           source: filePath,
@@ -52,13 +79,22 @@ function validateTagsForFiles (fileList, options) {
     })
 
     return Promise.all(getValidateTagsResults)
-  }).then(prepareReturnValue(formatters[options.f]))
+  })
+}
+
+function validateTagsForFiles (options) {
+  let files = options._
+  if (typeof files === 'string') {
+    files = [files]
+  }
+  const validated = files.map(validateTagsForFile)
+  return Promise.all(validated).then(prepareReturnValue(formatters[options.f]))
 }
 
 Promise.resolve().then(() => {
-  const args = minimist(process.argv.slice(2))
-  args.f = args.f || 'string'
-  return validateTagsForFiles(process.argv[2], args)
+  const args = minimist(process.argv.slice(2)) // eslint-disable-line no-magic-numbers
+  args.f = args.f || 'string'  // eslint-disable-line id-length
+  return validateTagsForFiles(args)
 }).then((linted) => {
   if (!linted.output) {
     return
